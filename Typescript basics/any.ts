bb6f9631007ac4698c1f3b50741b340465709e41
hiken056 <stefanrusu60@gmail.com>
2024-02-05 14:22:52 +0200

    typescript basics4

--- Typescript basics/any.ts
+++ Typescript basics/any.ts
@@ -1,6 +1,16 @@
 // ANY
 
-let fruitShop;
+let fruitShop: any;
 
+fruitShop.getFruit();
 
 console.log(fruitShop.toUpperCase());
+
+//nu da eroare chiar daca nu exista
+//avoid this type!!!!!
+
+const getGameStats = <T>(args:T) : void => {
+    
+}
+
+getGameStats<number>(45)
