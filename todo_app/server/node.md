6101bc53cc1ad8de567ad7110055be6cf988a1e6
hiken056 <stefanrusu60@gmail.com>
2024-02-07 20:57:24 +0200

    server 3

---
+++ todo_app/server/node.md
@@ -0,0 +1,9 @@
+CRUD => CREATE READ UPDATE DELETE
+
+/ => read all the note/tasks
+
+/ => create (post) => create a note/tasks
+
+/:uniqueId => read (get) => send the node related to this uniqueId
+/:uniqueId => update (post/put/patch) => update the node related to this uniqueId
+/:uniqueId => delete (delete) => delete the node related to this uniqueId
--- todo_app/server/src/index.ts
+++ todo_app/server/src/index.ts
@@ -2,12 +2,20 @@
 
 //comenzi pana aici: npm init, tsc --init, npm install express --save,  npm i @types/express --save-dev,  npm i @types/node --save-dev
 
+import exp from 'constants';
 import express from 'express'
 
 const app = express(); //express object
 
-app.get('/', (request, response) => { //get method (route, request, response)
-    response.send('<h1>Hello World</h1>')
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.post('/', (req, res) => { //get method (route, request, response)
+    //res.send('<h1>Hello World</h1>')
+
+    //here we need data so that we can create new note/todo
+    console.log(req.body)
+    res.json({message: "I am listening!"})
 });
 
 // LISTEN TO SOME PORT
