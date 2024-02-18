b35ad512086c338a047c61602027744845531403
hiken056 <stefanrusu60@gmail.com>
2024-02-18 16:06:53 +0200

    react state

--- todo_app/frontend/src/App.tsx
+++ todo_app/frontend/src/App.tsx
@@ -1,19 +1,26 @@
 import NoteItem from "./components/NoteItem";
+import React, { useState } from "react";
 
 const App = () => {
+  const [title, setTitle] = useState("default");
+
   return (
     <div className="max-w-3xl mx-auto space-y-6">
       <div className=" space-y-6  bg-white shadow-md rounded p-5">
         <h1 className=" font-semibold text-2xl text-center">Note Aplication</h1>
         <div>
+          {title.trim() && title.length < 4 ? (
+            <p className="text-red-500">Title is too short</p>
+          ) : null}
           <input
             type="text"
             className="w-full border-b-2 border-gray-700 outline-none"
             placeholder="Title"
+            onChange={(evt) => {
+              setTitle(evt.target.value);
+            }}
           />
         </div>
-
-
 
         <div>
           <textarea
@@ -28,15 +35,13 @@ const App = () => {
         </div>
       </div>
 
-      <NoteItem title="My first re-usable component"/>
+      <NoteItem title="My first re-usable component" />
 
-      <NoteItem title="Lorem"/>
+      <NoteItem title="Lorem" />
 
-      <NoteItem title="YAsss"/>
+      <NoteItem title="YAsss" />
 
-      <NoteItem title="biscuiti"/>
-
-      
+      <NoteItem title="biscuiti" />
     </div>
   );
 };
